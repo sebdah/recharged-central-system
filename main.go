1f11f0a8e0319cfd9794c3ab7ce0cdadc547f7c4
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-05-17 14:47:28 +0200

    Started a basic websockets server

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"github.com/op/go-logging"
+	"fmt"
+	"net/http"
+
+	goLogging "github.com/op/go-logging"
 	"github.com/sebdah/recharged-central-system/config"
+	"github.com/sebdah/recharged-central-system/logging"
+	"github.com/sebdah/recharged-shared/websockets"
 )
 
-var log = logging.MustGetLogger("main")
+var (
+	log      = goLogging.MustGetLogger("main")
+	WsServer *websockets.Server
+)
 
 func main() {
 	// Configure logging
-	setupLogging()
+	logging.Setup()
 
 	// Welcome message
 	log.Info("Starting re:charged central system")
 	log.Info("Environment: %s", config.Env)
+
+	// Setup Websockets endpoint
+	WsServer = websockets.NewServer()
+	http.HandleFunc("/ocpp-2.0j/ws", WsServer.Handler)
+
+	// Start the HTTP server
+	log.Info("Starting webserver on port %d", config.Config.GetInt("port"))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.GetInt("port")), nil)
+	if err != nil {
+		panic(err)
+	}
 }
