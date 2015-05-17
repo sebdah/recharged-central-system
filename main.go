eed3a556a365328d46f993d097514ee55556aafb
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-05-17 14:39:26 +0200

    Splitted out logging configuration to it's own file

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/op/go-logging"
 	"github.com/sebdah/recharged-central-system/config"
 )
@@ -16,17 +14,4 @@ func main() {
 	// Welcome message
 	log.Info("Starting re:charged central system")
 	log.Info("Environment: %s", config.Env)
-}
-
-// Configure logging
-func setupLogging() {
-	// Create a logging backend
-	backend := logging.NewLogBackend(os.Stderr, "", 0)
-
-	// Set formatting
-	format := logging.MustStringFormatter("%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}")
-	backendFormatter := logging.NewBackendFormatter(backend, format)
-
-	// Use the backends
-	logging.SetBackend(backendFormatter)
 }
